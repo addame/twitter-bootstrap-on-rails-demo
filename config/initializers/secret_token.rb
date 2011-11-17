e147eaebd8c56df1ee89aa8ea0c7774a7879c8c4
lucapette <lucapette@gmail.com>
2011-11-17 18:55:38 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterBootstrapOnRailsDemo::Application.config.secret_token = 'f88bc8fddb601f7ed4a7b3d9d162ff437c098a0b305c51fd4093ed29bec7d55c3a605eeb2a9e37760fc923450c2ce9e3dd7c7d41f4c7eaacc93646ae10806c6d'
